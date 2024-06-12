d594a2727faddf85cf567c905ec0ab83447b742e
inkfarer <inkfarer@inkfarer.com>
2024-06-12 17:25:00 +0300

    Remove unneeded replicant init

--- src/dashboard/entrant-import.ts
+++ src/dashboard/entrant-import.ts
@@ -3,13 +3,11 @@ import './styles/common.scss';
 import { createApp } from 'vue';
 import EntrantImportPanel from './pages/EntrantImport/EntrantImportPanel.vue';
 import { createPinia } from 'pinia';
-import { initEntrantStore } from 'client-shared/store/EntrantStore';
 import { setUpErrorHandler } from './helpers/ErrorHandlerStore';
 
 (async () => {
     const app = createApp(EntrantImportPanel);
     app.use(createPinia());
     setUpErrorHandler(app);
-    await initEntrantStore();
     app.mount('#app');
 })();
