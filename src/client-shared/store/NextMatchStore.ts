3563fef597c9f1683c25e58e927fc1bd3b8cc829
inkfarer <inkfarer@inkfarer.com>
2025-01-12 21:33:04 +0200

    Tweak appearance of bottom bar graphic, allowing for the flavor text to be hidden

--- src/client-shared/store/NextMatchStore.ts
+++ src/client-shared/store/NextMatchStore.ts
@@ -1,6 +1,7 @@
 import { NextMatch, StreamMatches } from 'types/schemas';
 import { defineStore } from 'pinia';
 import { createReplicantStoreInitializer } from 'client-shared/store/StoreHelper';
+import { formatPlayType } from 'client-shared/helpers/StringHelper';
 
 const nextMatch = nodecg.Replicant<NextMatch>('nextMatch');
 const streamMatches = nodecg.Replicant<StreamMatches>('streamMatches');
@@ -15,6 +16,9 @@ export const useNextMatchStore = defineStore('nextMatch', {
         nextMatch: null,
         streamMatches: null
     } as unknown as NextMatchStore),
+    getters: {
+        formattedPlayType: state => formatPlayType(state.nextMatch.match.playType, state.nextMatch.match.numberOfGames)
+    },
     actions: {
         setShowOnStream(newValue: boolean) {
             if (nextMatch.value) {
